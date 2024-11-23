ec83a66a65233a06527b0ca65ff5bfc1eb064b96
Bruno Marques <b_marques85@live.com>
2024-11-23 14:08:20 -0300

    Adicionar arquivos de projeto.

---
+++ Data/PersonContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Person.Models;
+using System.Security.Principal;
+
+namespace Person.Data
+{
+    public class PersonContext : DbContext
+    {
+        public DbSet<PersonModel> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(connectionString:"Data Source=person.sqlite");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+
+    
+}
